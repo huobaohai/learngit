532ca13afd13ecc5c91829c00c1d662d245a7de1
huobaohai <1074812590@qq.com>
2018-09-27 14:02:45 +0800

    py

---
+++ git_test_jenkins.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-09-05 15:23:35
+# @Author  : hbh (${email})
+# @Link    : ${link}
+# @Version : $Id$
+
+import unittest
+
+class TestStringMethod(unittest.TestCase):
+	
+	def test_upper(self):
+		self.assertEqual('foo'.upper(),'FOO')
+
+	def test_isupper(self):
+		self.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+	def test_split(self):
+		s = 'hello world'
+		self.assertEqual(s.split(),['hello','world'])
+		# check that s.split fails when the separator is not a string
+		# 如果split的参数不是string（比如2）的时候，抛出TypeError
+		with self.assertRaises(TypeError):
+			s.split(2)
+
+if __name__ == '__main__':
+	unittest.main()
